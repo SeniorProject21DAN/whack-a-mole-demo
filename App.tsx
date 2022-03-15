6afdbb9b9a5ef9e0893d8977b80419310e4c5ce3
nstrain <nstrain15@gmail.com>
2022-03-14 23:29:18 -0400

    add basic chromecast changes

--- App.tsx
+++ App.tsx
@@ -1,4 +1,8 @@
 /* add in chrome cast */
+// to run follow these instructions
+//    https://docs.expo.dev/workflow/customizing/
+// if yarn fails integrity test https://www.seancdavis.com/posts/fix-yarn-integrity-check-failed/
+// don't mix package managers or life sucks
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -7,14 +11,21 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+import { useRemoteMediaClient } from 'react-native-google-cast';
+import { CastButton } from 'react-native-google-cast';
+
+
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const client = useRemoteMediaClient();
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
+      <CastButton style={{ tintColor: 'black', width: 44, height: 44 }} />
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         {/* <StatusBar /> */}
