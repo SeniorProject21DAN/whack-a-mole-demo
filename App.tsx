87b1a3253524b696eb2ef699e7db4245ff58f649
nstrain <nstrain15@gmail.com>
2022-04-14 18:29:53 -0400

    working cast

--- App.tsx
+++ App.tsx
@@ -6,18 +6,25 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+import { useRemoteMediaClient } from 'react-native-google-cast';
+import { CastButton } from 'react-native-google-cast';
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const client = useRemoteMediaClient();
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        {/* <StatusBar /> */}
-      </SafeAreaProvider>
+      <React.Fragment>
+        <CastButton style={{ tintColor: 'black', width: 44, height: 44 }} />
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          {/* <StatusBar /> */}
+        </SafeAreaProvider>
+      </React.Fragment>
     );
   }
 }
